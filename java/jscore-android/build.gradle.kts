27528d62dc4e0d5b1e7ebe237827459ad30d2e06
Susan Cheng <susan.doggie@gmail.com>
2025-10-10 23:18:15 +0800

    Update build.gradle.kts

--- java/jscore-android/build.gradle.kts
+++ java/jscore-android/build.gradle.kts
@@ -89,6 +89,7 @@ tasks.preBuild {
             if (!localPropertiesFile.exists()) {
                 localPropertiesFile.createNewFile()
             }
+            println("Copying local.properties from $parentLocalPropertiesFile project to $localPropertiesFile")
             parentLocalPropertiesFile.copyTo(localPropertiesFile, true)
             break
         }
